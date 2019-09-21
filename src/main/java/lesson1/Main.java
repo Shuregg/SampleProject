a1b904982de7fc7aca05ef1757edb2d19fd10216
Александр Инников <san4ows55@gmail.com>
2019-09-21 13:30:43 +0300

    Java all types

---
+++ src/main/java/lesson1/Main.java
@@ -0,0 +1,17 @@
+package lesson1;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Alloha world");
+        int number = 0; //4Bite
+        float floatnumber = 3.14f; //4Bite
+        char a = 'a';
+        boolean booleanFlag = false; // 4-8bit;
+        byte b = 127; //1bit
+        short sh = 32767; //4Bite
+        long longnumber = 54775807; //8|Bite
+        double doublenumber = 1665.53; //8Bite
+    }
+
+}
