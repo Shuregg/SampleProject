d87255d1eb73e88d6c879d89a3205204f1a62e36
Александр Инников <san4ows55@gmail.com>
2019-09-21 14:07:41 +0300

    First task

---
+++ src/main/java/lesson1/Justice.java
@@ -0,0 +1,23 @@
+package lesson1;
+
+import java.util.Scanner;
+
+public class Justice {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number of family members");
+        int famnum = sc.nextInt();
+        System.out.println("Enter number of money");
+        int balance = sc.nextInt();
+        System.out.println("Enter the price of product");
+        int price = sc.nextInt();
+        if((price * famnum) <= money) {
+            balance = (balance - (price * famnum));
+            System.out.println("You can buy it for Your family! Your money: " + balance);
+        }
+        else
+        {
+            System.out.println("You can not buy it for Your family");
+        }
+    }
+}
