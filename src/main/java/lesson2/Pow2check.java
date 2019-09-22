615c3dbf3e7da44731d18b73c2e4f9b136743d2b
Александр Инников <san4ows55@gmail.com>
2019-09-22 11:33:26 +0300

    Number checking 2pow

---
+++ src/main/java/lesson2/Pow2check.java
@@ -0,0 +1,22 @@
+package lesson2;
+
+import java.util.Scanner;
+
+    public class Pow2check {
+        public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+            int a = sc.nextInt();
+            boolean x = pow2Check(a);
+            System.out.println(x);
+        }
+        public static boolean pow2Check(int num) {
+            int i = 0;
+            while(Math.pow(2, i) <= num) {
+                if ((Math.pow(2, i)) == num) {
+                    return true;
+                }
+            i++;
+            }
+    return false;
+        }
+    }
