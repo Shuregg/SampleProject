f2c80639fa1dfac6e223af3be3561b7b57fe387a
Александр Инников <san4ows55@gmail.com>
2019-09-22 10:40:20 +0300

    The smaller number (lesson2)

---
+++ src/main/java/lesson1/Numbers.java
@@ -0,0 +1,20 @@
+package lesson1;
+
+import java.util.Scanner;
+
+public class Numbers {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Write the 1st number.");
+        int a = sc.nextInt();
+        System.out.println("Write the 2nd number.");
+        int b = sc.nextInt();
+        System.out.println("Write the 3rd number.");
+        int c = sc.nextInt();
+        int min = 0;
+      if(a <= b && a <= c) min = a;
+      if(b <= a && b <= c) min = b;
+      if(c <= a && c <= b) min = c;
+        System.out.println("Minimum number: " + min);
+    }
+}
