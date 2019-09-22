e716143401b7ca59a453f7c410782ec684326e4e
Александр Инников <san4ows55@gmail.com>
2019-09-22 12:11:32 +0300

    multiplication (not ready)

---
+++ src/main/java/lesson2/MultiplicateToAnswer.java
@@ -0,0 +1,12 @@
+package lesson2;
+
+public class MultiplicateToAnswer {
+    public static void main(String[] args) {
+
+    }
+    public static int multiCheck(int a, int b, int answer) {
+        if((a * b) > answer) {
+
+        }
+    }
+}
