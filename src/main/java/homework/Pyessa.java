af6268511eb5c15de5ba8466af2b2ddea77450c7
Alexander <san4ows55@gmail.com>
2019-10-02 21:33:51 +0300

    Homework done(mb)

---
+++ src/main/java/homework/Pyessa.java
@@ -0,0 +1,29 @@
+package homework;
+
+import java.util.Scanner;
+    public class Pyessa {
+        public static void printTextPerRole(String[] roles, String[] textLines) {
+            for (int i = 0; i < roles.length ; i++) {
+                System.out.println(roles[i]);
+                for (int j = 0; j < textLines.length; j++) {
+                    if(textLines[j].startsWith(roles[i])) {
+                        System.out.println('\n' + j - 9 + ") " + textLines[j]);
+                    }
+                }
+            }
+        }
+        public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+            String[] cast = {"Городничий", "Аммос Федорович", "Артемий Филиппович", "Лука Лукич"};
+            //////////////////////////////////////////////////////////////////////////////////////
+            String[] text = {"Городничий: Я пригласил вас, господа, с тем, чтобы сообщить вам пренеприятное известие: к нам едет ревизор.",
+                             "Аммос Федорович: Как ревизор?",
+                             "Артемий Филиппович: Как ревизор?",
+                             "Городничий: Ревизор из Петербурга, инкогнито. И еще с секретным предписаньем.",
+                             "Аммос Федорович: Вот те на!",
+                             "Артемий Филиппович: Вот не было заботы, так подай!",
+                             "Лука Лукич: Господи боже! еще и с секретным предписаньем!"};
+            printTextPerRole(cast, text);
+        }
+ 
+}
