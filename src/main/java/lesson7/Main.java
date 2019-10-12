0949332f4f2e18bc2f2226c1cbf412f872777215
Александр Инников <san4ows55@gmail.com>
2019-10-12 14:23:25 +0300

    hlp pls

---
+++ src/main/java/lesson7/Main.java
@@ -0,0 +1,27 @@
+package lesson7;
+
+import java.util.List;
+
+public class Main {
+
+    static int designerPdfViewer(int[] h, String word) {
+        int w = word.length();
+        int max = 0;
+        for (int i = 0; i < word.length(); i++) {
+            for (int j = 0; j < h.length; j++) {
+
+            }
+        }
+        int s = max * w;
+        return s;
+    }
+    static int migratoryBirds(List<Integer> arr) {
+        for (int i = 0; i < arr.; i++) {
+
+        }
+    }
+    public static void main(String[] args) {
+        System.out.println((int)'b');
+    }
+}
+
