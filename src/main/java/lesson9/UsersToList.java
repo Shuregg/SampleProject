d654ce43df2725b13cff01e26278d7d2e37145e6
Александр Инников <san4ows55@gmail.com>
2019-10-19 14:29:40 +0300

    UsersFromFileToList

---
+++ src/main/java/lesson9/UsersToList.java
@@ -0,0 +1,30 @@
+package lesson9;
+
+
+import lesson8.lilApp.User;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+
+public class UsersToList {
+    public static void main(String[] args) {
+        try (FileReader reader = new FileReader("src\\main\\resources\\users.txt")) {
+            List<User> users = new ArrayList<>();
+            Scanner sc = new Scanner(reader);
+        while(sc.hasNext()) {
+            String login = sc.nextLine();
+            String password = sc.nextLine();
+            String name = sc.nextLine();
+            User user = new User(login, password, name);
+            users.add(user);
+        }
+
+        } catch (IOException e) {
+            System.out.println("Ошибка при чтении из файла\n" + e.getMessage());
+        }
+
+    }
+}
