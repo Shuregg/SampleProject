835303b98a45cda9c6dec9c0bc4ece451a2d4db6
Александр Инников <san4ows55@gmail.com>
2019-10-19 14:40:06 +0300

    ready reader for USERs

--- src/main/java/lesson9/UsersToList.java
+++ src/main/java/lesson9/UsersToList.java
@@ -6,24 +6,30 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.NoSuchElementException;
 import java.util.Scanner;
 
 
 public class UsersToList {
     public static void main(String[] args) {
+        User user;
+        List<User> users = new ArrayList<>();
         try (FileReader reader = new FileReader("src\\main\\resources\\users.txt")) {
-            List<User> users = new ArrayList<>();
             Scanner sc = new Scanner(reader);
         while(sc.hasNext()) {
             String login = sc.next();
             String password = sc.next();
             String name = sc.next();
-            User user = new User(login, password, name);
+            user = new User(login, password, name);
             users.add(user);
         }
-
         } catch (IOException e) {
-            System.out.println("Ошибка при чтении из файла\n" + e.getMessage());
+            System.err.println("Ошибка при чтении из файла\n" + e.getMessage());
+        } catch (NoSuchElementException e) {
+            System.err.println("Неверный формат данных\n");
+        }
+        for (User x : users) {
+            System.out.println(x);
         }
 
     }
