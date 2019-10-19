1228f4c445f305ceaa8bb390e61c289ce0ac915d
Александр Инников <san4ows55@gmail.com>
2019-10-19 14:43:24 +0300

    filehelper

---
+++ src/main/java/lesson8/lilApp/FileHelper.java
@@ -0,0 +1,11 @@
+package lesson8.lilApp;
+
+public class FileHelper {
+    public static Map<String,User> readFromFile(String path){
+
+    return null;
+}
+    public static void saveToFile(String path, Map<String,User> users){
+
+    }
+}
