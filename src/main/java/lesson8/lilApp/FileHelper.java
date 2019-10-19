417538a702af0862cd856bf6adcc3de6f47ddcdd
Alexander <san4ows55@gmail.com>
2019-10-19 21:52:54 +0300

    hw starting

--- src/main/java/lesson8/lilApp/FileHelper.java
+++ src/main/java/lesson8/lilApp/FileHelper.java
@@ -1,13 +1,29 @@
 package lesson8.lilApp;
 
-import java.util.Map;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.*;
 
 public class FileHelper {
-    public static Map<String,User> readFromFile(String path){
+    public static Map<String, User> readFromFile(String path) {
+        Map<String, User> users = new HashMap<>();
+        try (FileReader reader = new FileReader("src\\main\\resources\\usersMap.txt")) {
+            Scanner sc = new Scanner(reader);
+            while (sc.hasNext()) {
+                String login = sc.next();
+                String password = sc.next();
+                String name = sc.next();
+                User user = new User(login, password, name);
+                users.put(user.getLogin(), user);
+            }
+        } catch (IOException e) {
+            System.err.println("Ошибка при чтении из файла\n" + e.getMessage());
+        } catch (NoSuchElementException e) {
+            System.err.println("Неверный формат данных\n");
+        }
 
-    return null;
-}
-    public static void saveToFile(String path, Map<String,User> users){
+    }
+    public static void saveToFile (String path, Map < String, User > users){
 
     }
 }
